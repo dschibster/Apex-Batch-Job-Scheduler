b34626a07b70aa4f280e3de53f9e63aed1f4618c
dschibster <dennis.grzyb@outlook.com>
2024-01-25 08:04:47 +0100

    update commands to sf

--- scripts/promote-latest-package-version.sh
+++ scripts/promote-latest-package-version.sh
@@ -6,6 +6,6 @@ PACKAGEVERSIONID=$( jq -r 'last(.packageAliases[])' sfdx-project.json )
 echo "Promoting latest package version"
 
 echo "sfdx force:package:version:promote -p PACKAGE_VERSION_ID --noprompt -v devhub"
-sfdx force:package:version:promote -p $PACKAGEVERSIONID --noprompt -v devhub
+sf package version promote -p $PACKAGEVERSIONID --noprompt -v devhub
 
 
